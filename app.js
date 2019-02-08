4ceba277af1950d3549ad8552862df0ee2486499
rostyk <rostyslav.doskochynskiy@gmail.com>
2019-02-08 12:24:02 +0200

    idgenerator

---
+++ app.js
@@ -0,0 +1,22 @@
+const app = (function () {
+    const symbol = 'qwertyuiopasdfghjklzxcvbnm1234567890';
+
+    const generateId = () => {
+        let newStr = symbol.split('');
+        let randomSymbol = '';
+        let generatedId = '';
+        for (let i = 0; i < 50; i++) {
+            let random = Math.floor(Math.random() * newStr.length);
+            randomSymbol += newStr[random];
+            generatedId += randomSymbol[i];
+        }
+        return generatedId;
+    };
+
+    return {
+        generateId
+    }
+}());
+
+
+//
